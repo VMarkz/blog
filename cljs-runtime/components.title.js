b4e03a216d66d7001b9ac30f0b136dff8d52e330
VMarkz <vitor.portela@bandtec.com.br>
2021-07-12 19:01:03 -0300

    Change shadow-cljs output

--- cljs-runtime/components.title.js
+++
@@ -1,6 +0,0 @@
-goog.provide('components.title');
-components.title.title = (function components$title$title(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1.title.","h1.title.",-1316805875),"Curriculum Vitae"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2.subtitle","h2.subtitle",-1180086880),"Vitor Marques"], null)], null);
-});
-
-//# sourceMappingURL=components.title.js.map
