b1fdf8be80adb3f7ec77175168e820f7deef812e
Vitor Markz <61842891+VMarkz@users.noreply.github.com>
2021-07-16 10:58:25 -0300

    Delete resources/public/cljs-runtime directory

--- resources/public/cljs-runtime/components.header.js
+++
@@ -1,6 +0,0 @@
-goog.provide('components.header');
-components.header.header = (function components$header$header(items){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.header","div.header",1964513620),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [components.title.title], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [components.links.links,items], null)], null);
-});
-
-//# sourceMappingURL=components.header.js.map
